13dd6c9594f6a11a230b88860e4038bcc4476c8e
S M Tawhid Al Hasain <tawhidofficial50@gmail.com>
2026-09-20 16:31:43 +0600

    4

---
+++ Class 1/YourWork/M-variable.c
@@ -0,0 +1,15 @@
+#include <stdio.h>
+
+int main() {
+    int a, b, c, d;
+    char A, B, C, D;
+    scanf("%c%d", &A, &a);
+    scanf("%c%d", &B, &b);
+    scanf("%c%d", &C, &c);
+    scanf("%c%d", &D, &d);
+    printf("%c=%d\n", A, a);
+    printf("%c=%d\n", B, b);
+    printf("%c=%d\n", C, c);
+    printf("%c=%d\n", D, d);
+    return 0;
+}

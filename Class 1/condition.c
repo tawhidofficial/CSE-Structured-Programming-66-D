161010fb376979c2767052604b0f855752acf5be
S M Tawhid Al Hasain <tawhidofficial50@gmail.com>
2026-09-20 16:52:03 +0600

    condition.c

--- Class 1/condition.c
+++ Class 1/condition.c
@@ -8,23 +8,39 @@ int main() {
     //odd or even
     int sum = a + b;
     if (sum % 2 == 0)
+    {
         printf("sum is even\n");
+    }
     else
+    {
         printf("sum is odd\n");
+    }
 //sub is positive,zero or negetive
     if (a - b > 0)
+    {
         printf("Sub is positive\n");
+    }    
     else if (a - b == 0)
+    {
         printf("sub is zero\n");
+    }
     else
+    {
         printf("sub is negetive\n");
+    }
 // compare
     if (a < b)
+    {
         printf("First is less than second\n");
+    }
     else if (a == b)
+    {
         printf("First is equal to second\n");
+    }
     else
+    {
         printf("First is greater than second\n");
+    }
 
     return 0;
 }
